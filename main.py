a0cd08582600df96d9d4fee29489c65e575a19d8
Steven Clontz <steven.clontz@gmail.com>
2023-11-04 00:30:23 +0000

    export traits and theorems as well

--- main.py
+++ main.py
@@ -5,22 +5,46 @@ with open("data/main.json") as main_file:
 sheets = [
     "spaces",
     "properties",
-    # "theorems",
-    # "traits"
+    "theorems",
+    "traits"
 ]
-# same as
-# sheets = ["spaces", "properties", "theorems", "traits"]
 
 for sheet in sheets:
     with open(f"data/main.{sheet}.csv", "w") as sheet_file:
         writer = csv.writer(sheet_file)
-        writer.writerow([
-            "Ref ID style",
-            "Ref ID",
-            "Ref title",
-            "Object title",
-            "Object ID"
-        ])
+        if sheet == "spaces":
+            writer.writerow([
+                "Ref ID style",
+                "Ref ID",
+                "Ref title",
+                "Space title",
+                "Space ID"
+            ])
+        elif sheet == "properties":
+            writer.writerow([
+                "Ref ID style",
+                "Ref ID",
+                "Ref title",
+                "Property title",
+                "Property ID"
+            ])
+        elif sheet == "theorems":
+            writer.writerow([
+                "Ref ID style",
+                "Ref ID",
+                "Ref title",
+                "Theorem ID"
+            ])
+        else: #traits
+            writer.writerow([
+                "Ref ID style",
+                "Ref ID",
+                "Ref title",
+                "Space ID",
+                "Space title",
+                "Property ID",
+                "Property title"
+            ])
         for obj in json_data[sheet]:
             for ref in obj["refs"]:
                 if "doi" in ref.keys():
@@ -40,4 +64,34 @@ for sheet in sheets:
                     ident = ref["wikipedia"]
                 else:
                     ident_style = "unknown"
-                writer.writerow([ident_style, ident, ref["name"], obj["name"], obj["uid"]])
+                if sheet == "spaces":
+                    writer.writerow([
+                        ident_style, #"Ref ID style",
+                        ident, #"Ref ID",
+                        ref["name"], #"Ref title",
+                        obj["name"], #"Space title",
+                        obj["uid"], #"Space ID"
+                    ])
+                elif sheet == "properties":
+                    writer.writerow([
+                        ident_style, #"Ref ID style",
+                        ident, #"Ref ID",
+                        ref["name"], #"Ref title",
+                        obj["name"], #"Property title",
+                        obj["uid"], #"Property ID"
+                    ])
+                elif sheet == "theorems":
+                    writer.writerow([
+                        ident_style, #"Ref ID style",
+                        ident, #"Ref ID",
+                        ref["name"], #"Ref title",
+                        obj["uid"], #"Theorem ID"
+                    ])
+                else: #traits
+                    writer.writerow([
+                        ident_style, #"Ref ID style",
+                        ident, #"Ref ID",
+                        ref["name"], #"Ref title",
+                        obj["space"], #"Space ID"
+                        obj["property"], #"Property ID"
+                    ])
